fc2ff1d47aeab49840de23bdd217801b16ea97aa
Phoenix1747 <apertured@public-files.de>
2022-04-26 20:20:08 +0200

    add screenshots

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,7 @@ let isoList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2022-04-25';
+const APP_VERSION = '2022-04-26';
 let localStorageAvailable = false;
 
 /*
