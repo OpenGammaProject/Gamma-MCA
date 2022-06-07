OpenGammaProject/Gamma-MCA

commit 5d46712ac3e23e2a200a8fb3ac08192b0d8c3e6a

add peak detection

assets/js/plot.js
@@ -21,6 +21,15 @@ function SpectrumPlot(divId) {
   this.shapes = [];
   this.annotations = [];
   this.editableMode = false;
+  this.peakConfig = {
+    enabled: false,
+    thres: 0.03,
+    lag: 150,
+    width: 4,
+    lines: [],
+    lastDataX: [],
+    lastDataY: [],
+  };
 
   /*
     Get An Array with Length == Data.length containing ascending numbers
@@ -81,7 +90,7 @@ function SpectrumPlot(divId) {
     for(const i in newData) { // Compute the central moving average
       const intIndex = parseInt(i); // Gotcha, I wasted sooo much time on this -_-
 
-      if (intIndex >= half && intIndex <= target.length - half) { // Shortcut
+      if (intIndex >= half && intIndex <= target.length - half - 1) { // Shortcut
         const remainderIndexFactor = length % 2;
 
         const addVal = target[intIndex+half-remainderIndexFactor];
@@ -112,6 +121,57 @@ function SpectrumPlot(divId) {
     return newData;
   };
   /*
+    Find and mark energy peaks by using two different moving averages
+  */
+  this.peakFinder = function(doFind = true) {
+    if (this.peakConfig.lines.length !== 0) {
+      for (const line of this.peakConfig.lines) {
+        this.toggleLine(line, "", false);
+      }
+    }
+
+    if (!doFind) {
+      return;
+    }
+
+    const shortData = this.peakConfig.lastDataY;
+    const longData = plot.computeMovingAverage(this.peakConfig.lastDataY, this.peakConfig.lag);
+
+    const maxVal = Math.max(...shortData);
+    const xAxisData = this.peakConfig.lastDataX;
+    let peakLines = [];
+
+    for (let i = 0; i < shortData.length; i++) {
+      if (shortData[i] - longData[i] > this.peakConfig.thres * maxVal)  {
+        peakLines.push(xAxisData[i]);
+      }
+    }
+
+    let values = 0;
+    let size = 0;
+
+    for (let i = 0; i < peakLines.length-1; i++) {
+      if (peakLines[i + 1] - peakLines[i] <= this.peakConfig.width) {
+        values += peakLines[i];
+        size++;
+      } else {
+        let result;
+
+        if (values == 0) {
+          result = peakLines[i];
+        } else {
+          result = values / size;
+        }
+
+        values = 0;
+        size = 0;
+
+        this.toggleLine(result, Math.round(result));
+        this.peakConfig.lines.push(result);
+      }
+    }
+  };
+  /*
     Convenient Wrapper, could do more in the future
   */
   this.resetPlot = function(spectrumData) {
@@ -126,7 +186,7 @@ function SpectrumPlot(divId) {
   /*
     Add a line
   */
-  this.toggleLine = function(energy, name, enabled) {
+  this.toggleLine = function(energy, name, enabled = true) {
     if (enabled) {
       const newLine = {
         type: 'line',
@@ -184,10 +244,9 @@ function SpectrumPlot(divId) {
   /*
     Clear annotations and shapes
   */
-  this.clearAnnos = function(dataObj) {
+  this.clearAnnos = function() {
     this.shapes = [];
     this.annotations = [];
-    this.updatePlot(dataObj);
   };
   /*
     Plot All The Data
@@ -224,7 +283,7 @@ function SpectrumPlot(divId) {
     /*
       Compute Background and Corrected Spectrum
     */
-    if (dataObj.background.length > 0){//== dataObj.data.length) {
+    if (dataObj.background.length == dataObj.data.length) {
       bgTrace = {
         name: 'Background Spectrum',
         stackgroup: 'data', // Stack line charts on top of each other
@@ -339,8 +398,8 @@ function SpectrumPlot(divId) {
         yanchor: "top",
         yref: "paper",
       }],
-      shapes: this.shapes,
-      annotations: JSON.parse(JSON.stringify(this.annotations)), // Copy array but do not reference
+      //shapes: this.shapes,
+      //annotations: JSON.parse(JSON.stringify(this.annotations)), // Copy array but do not reference
     };
     /*
       Set calibrated x-axis
@@ -351,9 +410,6 @@ function SpectrumPlot(divId) {
       }
       layout.xaxis.title = 'Energy [keV]';
       layout.xaxis.ticksuffix = ' keV';
-      for (const anno of layout.annotations) {
-        anno.hovertext += layout.xaxis.ticksuffix;
-      }
     }
     /*
       CPS enabled
@@ -373,6 +429,29 @@ function SpectrumPlot(divId) {
       },
       editable: this.editableMode,
     };
+
+    /*
+      Peak Detection Stuff
+    */
+    if (this.peakConfig.enabled) {
+      if (data.length == 1) {
+        this.peakConfig.lastDataX = data[0].x;
+        this.peakConfig.lastDataY = data[0].y;
+      } else {
+        this.peakConfig.lastDataX = data[data.length - 1].x;
+        this.peakConfig.lastDataY = data[data.length - 1].y;
+      }
+      this.peakFinder();
+    }
+
+    layout.shapes = this.shapes;
+    layout.annotations = JSON.parse(JSON.stringify(this.annotations)); // Copy array but do not reference
+
+    if (this.calibration.enabled) {
+      for (const anno of layout.annotations) {
+        anno.hovertext += layout.xaxis.ticksuffix;
+      }
+    }
 
     if (update) {
       layout['uirevision'] = true;

assets/js/main.js
@@ -9,12 +9,12 @@
   Possible Future Improvements:
     - Sorting isotope list
     - Social media share function
-    - Peak Finder/Analyzer
     - (?) Add serial EOL char selection
     - FWHM calculation for peaks
     - (?) Serial console read capability
     - Hotkey to open/close settings
     - (?) Add desktop notifications
+    - Customizable Peak Detection Parameters
 
   Known Performance Issues:
     - Isotope hightlighting
@@ -58,7 +58,7 @@ let isoList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2022-05-14';
+const APP_VERSION = '2022-05-21';
 let localStorageAvailable = false;
 let firstInstall = false;
 
@@ -289,7 +289,7 @@ function fixHeight(parentId, contentId) {
 function sizeCheck() {
   const viewportWidth = document.documentElement.clientWidth;
   const viewportHeight = document.documentElement.clientHeight;
-  if (viewportWidth < 1200 || viewportHeight < 750) {
+  if (viewportWidth < 1250 || viewportHeight < 750) {
     popupNotification('screen-size-warning');
   } else {
     hideNotification('screen-size-warning');
@@ -333,6 +333,8 @@ function resetPlot() {
   if(plot.sma) {
     toggleSma(false, document.getElementById('sma'));
   }
+  plot.clearAnnos();
+  loadIsotopes(true);
   plot.resetPlot(spectrumData);
   bindPlotEvents(); // Fix Reset Bug: Hovering and Clicking not working.
 }
@@ -647,7 +649,8 @@ async function loadIsotopes(reload = false) { // Load Isotope Energies JSON ONCE
 
       const tableElement = document.getElementById('iso-table');
       tableElement.innerHTML = ''; // Delete old table
-      plot.clearAnnos(spectrumData); // Delete all isotope lines
+      plot.clearAnnos(); // Delete all isotope lines
+      plot.updatePlot(spectrumData);
 
       let intKeys = Object.keys(json);
       intKeys.sort((a, b) => a - b); // Sort Energies numerically
@@ -743,7 +746,7 @@ async function closestIso(value) {
       prevIso = newIso;
     }
 
-    plot.toggleLine(parseFloat(closest).toFixed(2), isoList[closest], true);
+    plot.toggleLine(parseFloat(closest).toFixed(2), isoList[closest]);
     plot.updatePlot(spectrumData);
   } else if (Object.keys(prevIso).length !== 0) {
     plot.toggleLine(Object.keys(prevIso)[0], Object.values(prevIso)[0], false);
@@ -777,6 +780,15 @@ function selectAll(selectBox) {
     plot.annotations = [];
   }
 
+  plot.updatePlot(spectrumData);
+}
+
+
+function findPeaks(checkbox) {
+  plot.peakConfig.enabled = checkbox.checked;
+  if (!checkbox.checked) { // disabled
+    plot.peakFinder(false);
+  }
   plot.updatePlot(spectrumData);
 }
 

index.html
@@ -332,7 +332,7 @@
       <div class="plot-tools row border border-top-0 border-1 border-secondary bg-light">
 
         <div class="col-md-auto d-md-flex">
-          <button class="btn btn-primary ps-2 pe-2 pt-1 pb-1 rounded-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas" title="Open Settings Menu" onclick="loadIsotopes();">
+          <button class="btn btn-primary ps-2 pe-2 pt-1 pb-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas" title="Open Settings Menu" onclick="loadIsotopes();">
             <i class="fas fa-sliders-h"></i> Settings
           </button>
         </div>
@@ -391,7 +391,7 @@
         <div class="col-md-auto pt-1 pb-1">
 
           <div class="input-group input-group-sm">
-            <span class="input-group-text">Plot Type</span>
+            <span class="input-group-text">Type</span>
             <button class="btn btn-primary btn-sm" type="button" id="plotType" onclick="changeType(this);" title="Switch plot type"><i class="fas fa-chart-line"></i> Line</button>
           </div>
 
@@ -418,6 +418,17 @@
 
           <input type="checkbox" class="btn-check" id="iso-hover" onclick="toggleIsoHover();" autocomplete="off">
           <label class="btn btn-outline-primary btn-sm" title="Show closest isotope when hovering (experimental)" for="iso-hover"> <i class="fa-solid fa-magnifying-glass"></i> Isotopes </label>
+
+        </div>
+
+        <div class="col-md-auto g-0">
+          <div class="vr h-100"></div>
+        </div>
+
+        <div class="col-md-auto pt-1 pb-1">
+
+          <input type="checkbox" class="btn-check" id="peak-finder" onclick="findPeaks(this);" autocomplete="off">
+          <label class="btn btn-outline-primary btn-sm" title="Find energy peaks (experimental)" for="peak-finder"> <i class="fa-solid fa-mountain"></i> Peaks </label>
 
         </div>
 
@@ -790,7 +801,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
-          Your window/screen size is smaller than recommended - therefore your experience may vary. For optimal usage increase the window size to a minimum of 1200x750 px.
+          Your window/screen size is smaller than recommended - therefore your experience may vary. For optimal usage increase the window size to a minimum of 1250 x 750 px.
         </div>
       </div>
 

service-worker.js
@@ -12,7 +12,7 @@
     - nothing.
 
 */
-const APP_VERSION = '2022-05-14';
+const APP_VERSION = '2022-05-21';
 const CACHE_NAME = "gamma-static"; // A random name for the cache
 const OFFLINE_RESOURCES = ['/',
                           '/index.html',

commit 3abd4891f161698f87d1874beed26495c630b01b

fix serial x-range issue
introduced with commit 5d46712ac3e23e2a200a8fb3ac08192b0d8c3e6a, broke 
serial recording :(

assets/js/plot.js
@@ -278,6 +278,8 @@ function SpectrumPlot(divId) {
       width: 1,
     };
 
+    let maxXValue = Math.max(...trace.x);
+
     let data = [trace];
 
     /*
@@ -289,7 +291,7 @@ function SpectrumPlot(divId) {
     /*
       Compute Background and Corrected Spectrum
     */
-    if (dataObj.background.length == dataObj.data.length) {
+    if (dataObj.background.length > 0) { //== dataObj.data.length)
       let bgTrace = {
         name: 'Background',
         stackgroup: 'data', // Stack line charts on top of each other
@@ -308,6 +310,10 @@ function SpectrumPlot(divId) {
           color: 'slategrey',
         },
       };
+
+      if (bgTrace.x.length > maxXValue) {
+        maxXValue = Math.max(...bgTrace.x);
+      }
 
       if (this.cps) {
         bgTrace.y = dataObj.backgroundCps;
@@ -335,8 +341,6 @@ function SpectrumPlot(divId) {
     /*
       All The Layout Stuff
     */
-    const maxXValue = Math.max(...trace.x);
-
     let layout = {
       uirevision: true,
       autosize: true, // Needed for resizing on update