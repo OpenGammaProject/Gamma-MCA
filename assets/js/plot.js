59826dcc1450b005400ffb6d5584c2c0331b0a00
Phoenix1747 <apertured@public-files.de>
2022-06-09 22:02:07 +0200

    scrollbar on some occasions

--- assets/js/plot.js
+++ assets/js/plot.js
@@ -513,7 +513,7 @@ function SpectrumPlot(divId) {
             <meta charset="utf-8">
           </head>
           <body style="margin:0;padding:0">
-            <div id="plotly-output" style="width:100vw;height:100vh"></div>
+            <div id="plotly-output" style="width:99vw;height:99vh"></div>
             <script src="${scriptUrl}"></script>
             <script type="text/javascript">Plotly.newPlot('plotly-output',${JSON.stringify(data)},${JSON.stringify(newLayout)},${JSON.stringify(newConfig)})</script>
           </body>
