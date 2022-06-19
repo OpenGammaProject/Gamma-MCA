ac939c91ff9e038f4bb104b609492e1d2ce21809
Phoenix1747 <apertured@public-files.de>
2022-06-19 22:17:59 +0200

    bump version

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@
     - (?) Serial console read capability
     - (?) Hotkey to open/close settings
     - (?) Add desktop notifications
-    - Add Isotope Recognition for Peak Detector
     - !!! Weird comb-structure with quadratic calibrations
     - !!! Check Calibration Regression
 
@@ -59,7 +58,7 @@ let isoList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2022-06-19';
+const APP_VERSION = '2022-06-20';
 let localStorageAvailable = false;
 let firstInstall = false;
 
