9053ddc56b10240f5d5bea28b5e1f071e5b55381
Phoenix1747 <apertured@public-files.de>
2022-08-03 22:35:25 +0200

    bump version

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ let isoList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2022-07-29';
+const APP_VERSION = '2022-08-03';
 let localStorageAvailable = false;
 let firstInstall = false;
 
