a66978835ece93d7af835c39af757d1af96dceb7
Phoenix1747 <apertured@public-files.de>
2022-09-08 17:52:36 +0200

    bump version

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
     - nothing.
 
 */
-const APP_VERSION = '2022-09-04';
+const APP_VERSION = '2022-09-08';
 const CACHE_NAME = "gamma-static"; // A random name for the cache
 const OFFLINE_RESOURCES = ['/',
                           '/index.html',
