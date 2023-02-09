a9df73398bfb08d707db0d4ed70c4672c550578b
Phoenix1747 <apertured@public-files.de>
2023-02-09 14:37:09 +0100

    formatting

--- source/external/webusbserial.d.ts
+++ source/external/webusbserial.d.ts
@@ -1,9 +1,9 @@
-type DataCallback = (data:Uint8Array)=>void;
-type ErrorCallback = (data:any)=>void;
+type DataCallback = (data: Uint8Array) => void;
+type ErrorCallback = (data: any) => void;
 
 export class WebUSBSerialPort {
-  constructor(device:any, portConfiguration:any);
-  connect(onData:DataCallback, onError:ErrorCallback):void;
-  send(data:string):void;
-  disconnect():void;
+  constructor(device: any, portConfiguration: any);
+  connect(onData: DataCallback, onError: ErrorCallback): void;
+  send(data: string): void;
+  disconnect(): void;
 }
