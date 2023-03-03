b89da1b59a47c00289cd164054c71504fab9e9db
Phoenix1747 <apertured@public-files.de>
2023-03-03 13:36:03 +0100

    bump version

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
   https://nuclearphoenix.xyz
 
 */
-const APP_VERSION = '2023-02-20';
+const APP_VERSION = '2023-03-03';
 const CACHE_NAME = 'gamma-static'; // A random name for the cache
 
 const OFFLINE_RESOURCES = ['/',
