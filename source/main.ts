6b69eba8b5c537743c12538b08cb4d509069b3ee
Phoenix1747 <nuclearphoenix@public-files.de>
2023-04-19 21:15:18 +0200

    bump version

--- source/main.ts
+++ source/main.ts
@@ -120,7 +120,7 @@ const isoList: IsotopeList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2023-03-27';
+const APP_VERSION = '2023-04-19';
 let localStorageAvailable = false;
 let fileSystemWritableAvail = false;
 let firstInstall = false;
