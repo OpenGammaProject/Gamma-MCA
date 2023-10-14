39f1dfcb7d553b4b7059eff990b0316ecc9eb1e0
NuclearPhoenixx <nuclearphoenix@public-files.de>
2023-10-14 22:09:44 +0200

    bump version

--- source/main.ts
+++ source/main.ts
@@ -118,7 +118,7 @@ const isoList: IsotopeList = {};
 let checkNearIso = false;
 let maxDist = 100; // Max energy distance to highlight
 
-const APP_VERSION = '2023-08-27';
+const APP_VERSION = '2023-10-14';
 const localStorageAvailable = 'localStorage' in self; // Test for localStorage, for old browsers
 const wakeLockAvailable = 'wakeLock' in navigator; // Test for Screen Wake Lock API
 let fileSystemWritableAvail = false;
