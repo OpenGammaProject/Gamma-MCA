75dc0186247a7947c659899cdf71999f57cc55f1
NuclearPhoenixx <nuclearphoenix@public-files.de>
2023-12-09 19:29:24 +0100

    update todo

--- source/main.ts
+++ source/main.ts
@@ -24,6 +24,8 @@
     - Automatically close system notifications when the user interacts with the page again
     - Optional real-time file saving to help with long recordings that might crash (create some temp files)
     - Analysis report generator (print via window.open(), print.html, window.print())
+    - CANNOT START RECORDING WITH CPS MODE ENABLED!
+    - NEW RECORDING STARTED -> REMOVE RECORDING STOPPED NOTIFICATIONS
 
   Known Issues/Problems/Limitations:
     - Plot.ts: Gaussian Correlation Filtering still has pretty bad performance despite many optimizations already.
