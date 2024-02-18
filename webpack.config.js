27c47997f4e449d03b3fd17c5a38c05b38f555b8
NuclearPhoenixx <nuclearphoenix@public-files.de>
2024-02-18 21:01:19 +0100

    fix webpack dev server path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/dist',
   },
   resolve: {
     extensions: ['.ts', '.js'],
