d5b9c0e5085068332d40ffbb78515e00ad810f04
NuclearPhoenixx <nuclearphoenix@public-files.de>
2024-02-19 20:31:32 +0100

    bump version

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
   https://nuclearphoenix.xyz
 
 */
-const APP_VERSION = '2024-02-04';
+const APP_VERSION = '2024-02-19';
 const CACHE_NAME = 'gamma-static'; // A random name for the cache
 
 const OFFLINE_RESOURCES = ['/',
